8efda9149dbd0138ba1d08c0a1ade0d8d905e051
ddvudo <644077730@qq.com>
2019-04-29 18:47:24 +0800

    :sparkles: Introducing new features.

--- WebContent/index.js
+++ WebContent/index.js
@@ -3,7 +3,24 @@ function initLeftTab() {
 }
 
 function renderTabList(arg) {
-    console.log(arg);
+    console.log(JSON.parse(arg));
+    var res = JSON.parse(arg);
+    if (res.state) {
+        var data = res.data;
+        for (var i = 0; i < data.length; i++) {
+            var li = document.createElement("li");
+            li.setAttribute("class", "layui-nav-item layui-nav-itemed");
+            var a = document.createElement("a");
+            a.innerHTML = data[i].cnname;
+            a.setAttribute("nodevalue", JSON.stringify(data[i]));
+            a.setAttribute("href", "javascript:;");
+            li.append(a);
+            $("#main-nav-bar").append(li);
+            layui.use("element(main-nav-bar)", function () {
+                layui.element.render("nav", "main-nav-bar");
+            })
+        }
+    }
 }
 window.onload = function(){
     initLeftTab();
