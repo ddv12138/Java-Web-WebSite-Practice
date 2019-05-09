49e1a28f110516c812bf05af5dc3a869a3b08bb4
ddvudo <644077730@qq.com>
2019-05-09 18:28:12 +0800

    :white_check_mark: Adding tests.

--- src/main/java/WebComponent/Controller/HelloController.java
+++ src/main/java/WebComponent/Controller/HelloController.java
@@ -1,12 +1,24 @@
 package WebComponent.Controller;
 
+import globalUtils.CommonResult;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Controller
 public class HelloController {
     @RequestMapping("/test")
     public String index() {
         return "index.html";
+    }
+
+    @RequestMapping("/getTabList")
+    @ResponseBody
+    public CommonResult getTabList(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        return new CommonResult(true, "", null);
     }
 }
