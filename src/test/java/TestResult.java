10a57228536bb57cb9f579b1ba145355d47e5a9a
ddvudo <644077730@qq.com>
2019-05-29 17:42:02 +0800

    :ok_hand: Updating code due to code review changes.

--- src/test/java/TestResult.java
+++ src/test/java/TestResult.java
@@ -1,10 +1,24 @@
-import globalUtils.CommonResult;
+import ORM.POJO.ResourceTable;
 import org.junit.Test;
+
+import java.util.Optional;
 
 
 public class TestResult {
     @Test
-    public void main(){
-        System.out.println(new CommonResult(true,"message",null));
+    public void TestOptional() {
+        ResourceTable res = new ResourceTable();
+        res.setId(0);
+        res.setName("asd");
+        res.setCnname("asdasd");
+        System.out.println(test(res));
+    }
+
+    private String test(ResourceTable t) {
+        return Optional.ofNullable(t).map(r -> {
+            System.out.println(r.getName());
+            System.out.println(t.getName());
+            return t.getName();
+        }).toString();
     }
 }
