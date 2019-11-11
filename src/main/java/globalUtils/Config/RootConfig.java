8f9fe989233e4bf0435fe5853f3e9f6073d850c2
ddvudo <644077730@qq.com>
2019-11-11 14:42:32 +0800

    :speech_balloon: Updating text and literals.
    Signed-off-by: ddvudo <644077730@qq.com>

--- src/main/java/globalUtils/Config/RootConfig.java
+++ src/main/java/globalUtils/Config/RootConfig.java
@@ -2,8 +2,10 @@ package globalUtils.Config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @Configuration
+@EnableAspectJAutoProxy(proxyTargetClass = true)
 @ComponentScan(basePackages = {"globalUtils.AspectJ"})
 public class RootConfig {
 }
--- src/main/java/globalUtils/Config/WebConfig.java
+++ src/main/java/globalUtils/Config/WebConfig.java
@@ -5,7 +5,6 @@ import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.ViewResolver;
@@ -19,7 +18,6 @@ import java.util.List;
 
 @Configuration
 @EnableWebMvc
-@EnableAspectJAutoProxy
 @ComponentScan(basePackages = {"WebComponent.Controller", "Services"})
 public class WebConfig implements WebMvcConfigurer {
 
