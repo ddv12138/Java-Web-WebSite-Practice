d8527a9751f212a6ecefe42c16e77b074b30d9b0
ddvudo <644077730@qq.com>
2019-11-22 18:13:42 +0800

    静态资源被拦截

--- src/main/java/GlobalUtils/Config/SecurityConfig.java
+++ src/main/java/GlobalUtils/Config/SecurityConfig.java
@@ -20,9 +20,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-				.antMatchers("/user/**", "/", "/spittr", "/layui/**", "/javascript/**",
-						"/img/**", "/house/**", "/font/**", "/EnterpriseInfomation/**",
-						"/css/**", "/resourceManage/**", "/signup/**", "/SSRLinkDection/**", "/zTree_v3/**").permitAll()
+				.antMatchers("/user/**", "/", "/spittr").permitAll()
+				.regexMatchers("\\*.css").permitAll()
+				.regexMatchers("\\*.js").permitAll()
 				.anyRequest().authenticated()
 				.and()
 				.formLogin()
