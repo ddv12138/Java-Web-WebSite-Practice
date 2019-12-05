4594069b10fbd8f6a9f49ace9dd8a557966f459c
ddvudo <644077730@qq.com>
2019-12-05 11:30:36 +0800

    redis缓存成谜

--- src/main/java/GlobalUtils/Config/RootConfig.java
+++ src/main/java/GlobalUtils/Config/RootConfig.java
@@ -1,7 +1,8 @@
 package GlobalUtils.Config;
 
-import GlobalUtils.CommonResult;
-import GlobalUtils.Global;
+import GlobalUtils.FastJsonRedisSerializer;
+import com.alibaba.fastjson.parser.ParserConfig;
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -14,58 +15,24 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisPassword;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import java.time.Duration;
 
 @Configuration
 @EnableCaching
 @ComponentScan(basePackages = {"WebComponent", "GlobalUtils"}, excludeFilters = {
-		@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = DataSourceConfig.class),
-		@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = RootConfig.class),
-		@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = SecurityConfig.class),
-		@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = WebAppInitializer.class),
-		@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = WebConfig.class),
-		@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = CommonResult.class),
-		@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = Global.class)
+		@ComponentScan.Filter(type = FilterType.ANNOTATION, value = Configuration.class)
 })
 public class RootConfig {
 	@Bean
-	public RedisCacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+	public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory, RedisCacheConfiguration redisCacheConfiguration) {
 		//初始化一个RedisCacheWriter
 		RedisCacheWriter redisCacheWriter = RedisCacheWriter.nonLockingRedisCacheWriter(redisConnectionFactory);
-		//序列化方式1
-		//设置CacheManager的值序列化方式为JdkSerializationRedisSerializer,但其实RedisCacheConfiguration默认就是使用StringRedisSerializer序列化key，JdkSerializationRedisSerializer序列化value,所以以下(4行)注释代码为默认实现
-//		ClassLoader loader = this.getClass().getClassLoader();
-//		JdkSerializationRedisSerializer jdkSerializer = new JdkSerializationRedisSerializer(loader);
-//		RedisSerializationContext.SerializationPair<Object> pair = RedisSerializationContext.SerializationPair.fromSerializer(jdkSerializer);
-//		RedisCacheConfiguration defaultCacheConfig = RedisCacheConfiguration.defaultCacheConfig().serializeValuesWith(pair);
-		//序列化方式1---另一种实现方式
-//		RedisCacheConfiguration defaultCacheConfig = RedisCacheConfiguration.defaultCacheConfig();//该语句相当于序列化方式1
-
-		//序列化方式2
-		Jackson2JsonRedisSerializer serializer = new Jackson2JsonRedisSerializer(Object.class);
-		RedisSerializationContext.SerializationPair<Object> pair = RedisSerializationContext.SerializationPair.fromSerializer(serializer);
-		RedisCacheConfiguration defaultCacheConfig = RedisCacheConfiguration.defaultCacheConfig().serializeValuesWith(pair);
-
-		//序列化方式3  JSONObject
-//		FastJsonRedisSerializer<Object> fastJsonRedisSerializer = new FastJsonRedisSerializer<>(Object.class);
-//		RedisSerializationContext.SerializationPair<Object> pair = RedisSerializationContext.SerializationPair.fromSerializer(fastJsonRedisSerializer);
-//		RedisCacheConfiguration defaultCacheConfig = RedisCacheConfiguration.defaultCacheConfig().serializeValuesWith(pair);
-		//设置过期时间 30天
-		defaultCacheConfig = defaultCacheConfig.entryTtl(Duration.ofDays(30));
 		//初始化RedisCacheManager
-		RedisCacheManager cacheManager = new RedisCacheManager(redisCacheWriter, defaultCacheConfig);
-		//设置白名单---非常重要********
-        /*
-        使用fastjson的时候：序列化时将class信息写入，反解析的时候，
-        fastjson默认情况下会开启autoType的检查，相当于一个白名单检查，
-        如果序列化信息中的类路径不在autoType中，
-        反解析就会报com.alibaba.fastjson.JSONException: autoType is not support的异常
-        可参考 https://blog.csdn.net/u012240455/article/details/80538540
-         */
-//		ParserConfig.getGlobalInstance().addAccept("ORM.POJO.");
+		RedisCacheManager cacheManager = new RedisCacheManager(redisCacheWriter, redisCacheConfiguration);
 		return cacheManager;
 	}
 
@@ -78,4 +45,50 @@ public class RootConfig {
 		return factory;
 	}
 
+	@Bean(name = "redisTemplate")
+	@SuppressWarnings("unchecked")
+	public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+		RedisTemplate<Object, Object> template = new RedisTemplate<>();
+		//使用fastjson序列化
+		FastJsonRedisSerializer fastJsonRedisSerializer = new FastJsonRedisSerializer(Object.class);
+		// value值的序列化采用fastJsonRedisSerializer
+		template.setValueSerializer(fastJsonRedisSerializer);
+		template.setHashValueSerializer(fastJsonRedisSerializer);
+		// key的序列化采用StringRedisSerializer
+		template.setKeySerializer(new StringRedisSerializer());
+		template.setHashKeySerializer(new StringRedisSerializer());
+		template.setConnectionFactory(redisConnectionFactory);
+		return template;
+	}
+
+	/**
+	 * 设置 redis 数据默认过期时间
+	 * 设置@cacheable 序列化方式
+	 *
+	 * @return
+	 */
+	@Bean
+	public RedisCacheConfiguration redisCacheConfiguration() {
+		//JDK方式
+//		RedisCacheConfiguration configuration = RedisCacheConfiguration.defaultCacheConfig();
+		//FastJson方式
+		FastJsonRedisSerializer<Object> fastJsonRedisSerializer = new FastJsonRedisSerializer<>(Object.class);
+		RedisCacheConfiguration configuration = RedisCacheConfiguration.defaultCacheConfig();
+		configuration = configuration.serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(fastJsonRedisSerializer)).entryTtl(Duration.ofDays(1));
+		//设置白名单---非常重要********
+        /*
+        使用fastjson的时候：序列化时将class信息写入，反解析的时候，
+        fastjson默认情况下会开启autoType的检查，相当于一个白名单检查，
+        如果序列化信息中的类路径不在autoType中，
+        反解析就会报com.alibaba.fastjson.JSONException: autoType is not support的异常
+        可参考 https://blog.csdn.net/u012240455/article/details/80538540
+         */
+		ParserConfig.getGlobalInstance().addAccept("ORM.POJO.");
+
+		//Jackson序列化方式
+//		Jackson2JsonRedisSerializer<Object> serializer = new Jackson2JsonRedisSerializer<>(Object.class);
+//		RedisSerializationContext.SerializationPair<Object> pair = RedisSerializationContext.SerializationPair.fromSerializer(serializer);
+//		RedisCacheConfiguration configuration = RedisCacheConfiguration.defaultCacheConfig().serializeValuesWith(pair);
+		return configuration;
+	}
 }
