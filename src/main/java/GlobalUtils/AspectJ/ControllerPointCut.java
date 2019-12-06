770f7c85511107c4dc57f0b7b6032e2cd2530bcb
ddvudo <644077730@qq.com>
2019-12-06 18:04:09 +0800

    去除一些不必要的警告

--- src/main/java/GlobalUtils/AspectJ/ControllerPointCut.java
+++ src/main/java/GlobalUtils/AspectJ/ControllerPointCut.java
@@ -17,7 +17,7 @@ public class ControllerPointCut {
 		Object obj = joinPoint.proceed(joinPoint.getArgs());
 		long end = System.currentTimeMillis();
 		StringBuilder sb = new StringBuilder();
-		sb.append("调用控制器：" + joinPoint.getSignature().getName() + ",");
+		sb.append("调用Controller：" + joinPoint.getSignature().getName() + ",");
 		sb.append("耗时：" + (end - start) + "ms" + "");
 		Global.Logger().info(sb.toString());
 		return obj;
