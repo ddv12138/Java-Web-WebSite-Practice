2d1b6389829f4891a0f234dd5fc62e14a4ee1eb6
ddvudo <644077730@qq.com>
2020-01-16 17:53:22 +0800

    试下高德新的可视化API

--- src/main/resources/static/sa-html/house/house.js
+++ src/main/resources/static/sa-html/house/house.js
@@ -1,70 +1,139 @@
 window.onload = function () {
     let cityStr = "武汉";
+    window.countScale = 10;
     $.post("/city/getCityInfo", {cityName: cityStr}, function (cityData, status) {
         cityData = JSON.parse(cityData.data).results[0];
         var map = new AMap.Map("container", {
+            mapStyle: 'amap://styles/1de318cbb8d12c02303a22c550b9ccc9',
+            viewMode: '3D',
+            pitch: 15,
             resizeEnable: true,
             center: [cityData.location.lng, cityData.location.lat],
-            zoom: 11
+            zoom: 12,
         });
-        $.post("/community/list", {cityName: cityStr}, function (commData, status) {
+        window.map = map;
+        $.post("/community/list", {cityName: cityStr}, function (commData) {
             let points = [];
+            let height = 0;
+            let viewMode = '3D';
             commData = commData.data;
             commData.forEach(function (loc) {
-                points.push({lng: loc.gaode_lng, lat: loc.gaode_lat, count: loc.unit_price});
-            });
-            console.log(points);
-            if (!isSupportCanvas()) {
-                alert('热力图目前只支持有canvas支持的浏览器,您所使用的浏览器不能使用热力图功能~')
-            }
-            var heatmap;
-            map.plugin(["AMap.Heatmap"], function () {
-                //初始化heatmap对象
-                heatmap = new AMap.Heatmap(map, {
-                    radius: 25, //给定半径
-                    opacity: [0, 0.8]
-                    /*,
-                    gradient:{
-                        0.5: 'blue',
-                        0.65: 'rgb(117,211,248)',
-                        0.7: 'rgb(0, 255, 0)',
-                        0.9: '#ffea00',
-                        1.0: 'red'
-                    }
-                     */
-                });
-                //设置数据集：该数据为北京部分“公园”数据
-                heatmap.setDataSet({
-                    data: points,
-                    max: 80000
+                if ((parseFloat(loc.unit_price) / window.countScale) > height) {
+                    height = (parseFloat(loc.unit_price) / window.countScale);
+                }
+                points.push({
+                    lng: loc.gaode_lng,
+                    lat: loc.gaode_lat,
+                    count: parseFloat(loc.unit_price) / window.countScale,
+                    lnglat: loc.gaode_lng + "," + loc.gaode_lat
                 });
             });
+            console.log(height);
+            initPointAdcode(points, height, viewMode);
         })
     })
 };
 
-function isSupportCanvas() {
-    var elem = document.createElement('canvas');
-    return !!(elem.getContext && elem.getContext('2d'));
+function initPointAdcode(points, height, viewMode) {
+    let mapLayer = new Loca.HexagonLayer({
+        map: window.map,
+        visible: true,
+        zIndex: 777,
+        // fitView: true,
+        eventSupport: true,
+    });
+    mapLayer.setData(points, {
+        type: 'json',
+        lnglat: function (obj) {
+            return [parseFloat(obj.value.lng), parseFloat(obj.value.lat)];
+        },
+        value: 'count'
+    });
+    mapLayer.setOptions({
+        unit: 'meter',
+        mode: 'mean',
+        style: {
+            radius: 100,
+            opacity: 0.9,
+            gap: 1,
+            height: [0, height],
+            // color: {
+            //     scale: 'quantize',
+            //     value: ['rgb(255,237,160)', 'rgb(254,217,118)', 'rgb(254,178,76)', 'rgb(253,141,60)', 'rgb(252,78,42)', 'rgb(227,26,28)', 'rgb(189,0,38)', ],
+            // }
+        }
+    }).render();
+    // mapLayer.setFitView();
+    // 事件 legendupdate: 图例数据更新完成回调此函数
+    mapLayer.on('legendupdate', function (ev) {
+        var colorLegend = ev.colorLegend;
+
+        var legends = colorLegend.map(item => {
+            // color 为 gradient 传入的格式
+            return `<li class="color-item" style="background-color: ${item.color}"></li>`
+        });
+
+        var ranges = colorLegend.map((item, index) => {
+            // range 可能为小数，可以自行取整计算
+            console.log(item.range);
+            item.range[0] = Math.ceil(item.range[0] * window.countScale);
+            item.range[1] = Math.ceil(item.range[1] * window.countScale);
+
+            if (index == colorLegend.length - 1) {
+                return `<li class="label-item">${item.range[0]}</li><li class="label-item">${item.range[1]}</li>`;
+            }
+            return `<li class="label-item">${item.range[0]}</li>`;
+        });
+
+        document.getElementById('legend-color').innerHTML = legends.join('');
+        document.getElementById('legend-label').innerHTML = ranges.join('');
+    });
+    mapLayer.on('mousemove', function (ev) {
+        updateInfo(ev);
+    });
+    onHeight(viewMode, mapLayer, height);
+    // initContourLayer(points);
 }
 
-function setGradient() {
-    /*格式如下所示:
-   {
-         0:'rgb(102, 255, 0)',
-         .5:'rgb(255, 170, 0)',
-         1:'rgb(255, 0, 0)'
-   }*/
-    var gradient = {
-        .1: '#82B8DA',
-        .2: '#66A9D7',
-        .3: '#4394C2',
-        .4: '#3B8BBE',
-        .5: '#2F7EB9',
-        .6: '#226DB0',
-        .7: '#125CA3',
-        .8: '#18639E',
-        .9: '#073067',
-    };
-    heatmapOverlay.setOptions({"gradient": gradient});
+function updateInfo(ev) {
+    console.log(ev);
+    let $val = document.getElementById('val');
+    let $lngLat = document.getElementById('lng-lat');
+    $val.innerText = (parseFloat(ev.rawData[0].count) * window.countScale) + "";
+    $lngLat.innerText = ev.lngLat.valueOf();
+}
+
+function initContourLayer(points) {
+    var contourLayer = new Loca.ContourLayer({
+        shape: 'isoline',
+        map: window.map
+    });
+    contourLayer.setData(points, {
+        type: 'json',
+        value: 'count'
+    });
+    contourLayer.setOptions({
+        smoothNumber: 3,
+        threshold: 50,
+        interpolation: {
+            step: 1000,
+            effectRadius: 800,
+        },
+    });
+    contourLayer.render();
+}
+
+function onHeight(viewMode, gridLayer, height) {
+    var heightBtn = document.querySelector('#heightBtn');
+
+    heightBtn.onclick = function () {
+        viewMode = viewMode === '2D' ? '3D' : '2D';
+        heightBtn.innerHTML = viewMode;
+
+        gridLayer.setOptions({
+            style: {
+                height: [0, viewMode === '2D' ? 0 : height]
+            }
+        }).render();
+    }
 }
