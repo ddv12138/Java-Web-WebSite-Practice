38370fe4019a59bc1cbe460211a80bbe00fb0aba
ddv12138 <644077730@qq.com>
2020-01-19 23:44:52 +0800

    百度地图坐标转高德

---
+++ src/test/java/DataTest.java
@@ -0,0 +1,46 @@
+import GlobalUtils.Application;
+import GlobalUtils.Global;
+import ORM.POJO.City;
+import ORM.POJO.Community;
+import WebComponent.Service.Services.CityService;
+import WebComponent.Service.Services.CommunityService;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
+
+@SpringBootTest(classes = Application.class)
+@RunWith(SpringJUnit4ClassRunner.class)
+public class DataTest {
+	@Autowired
+	CityService cityService;
+	@Autowired
+	CommunityService communityService;
+
+	@Test
+	public void insertGaoDeLngAngLat() {
+		String citystr = "厦门";
+		City city = cityService.selectByName(citystr);
+		List<Community> communities = communityService.selectHetMapDataByCity(city);
+		for (Community community : communities) {
+			String url = "https://restapi.amap.com/v3/assistant/coordinate/convert?" +
+					"locations=" + community.getLongitude() + "," + community.getLatitude() + "&" +
+					"coordsys=baidu&" +
+					"output=json&" +
+					"key=5e842e2d890e0361743c15a6e1ec168a";
+			String resStr = Global.doGetHttpRequest(url);
+			JSONObject res = JSON.parseObject(resStr);
+			if (res.getIntValue("status") == 1) {
+				community.setGaode_lng(res.getString("locations").split(",")[0]);
+				community.setGaode_lat(res.getString("locations").split(",")[1]);
+				int i = communityService.updateOneLoc(community);
+				Global.Logger(this).info(communities.size() + "/" + communities.indexOf(community));
+			}
+		}
+	}
+}
