7f3c119030bd0d087d17a9a4f9e0b1240276d1b4
ddv12138 <644077730@qq.com>
2020-04-02 18:35:13 +0800

    疫情数据处理

--- src/test/java/ScheduledTask/NcovDataTaskTest.java
+++ src/test/java/ScheduledTask/NcovDataTaskTest.java
@@ -5,6 +5,10 @@ import GlobalUtils.Global;
 import ORM.Mapper.NcovMapper;
 import ORM.POJO.Ncov;
 import com.alibaba.fastjson.JSONArray;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+import org.apache.commons.io.input.BOMInputStream;
 import org.junit.Ignore;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
@@ -15,6 +19,11 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -30,17 +39,30 @@ class NcovDataTaskTest {
 	NcovMapper ncovMapper;
 
 	@Test
-	void insertData() {
-		List<Ncov> data = new LinkedList<>();
-		for(int i=0;i<100;i++){
-			Ncov ncov = new Ncov();
-			ncov.setCity(i+"");
-			ncov.setCitycode(i+"");
-			ncov.setConfirmed(i+"");
-			ncov.setCountry(i+"");
-			ncov.setProvince(i+"");
-			data.add(ncov);
+	void insertData() throws IOException {
+//		List<Ncov> data = new LinkedList<>();
+//		for(int i=0;i<100;i++){
+//			Ncov ncov = new Ncov();
+//			ncov.setCity(i+"");
+//			ncov.setCitycode(i+"");
+//			ncov.setConfirmed(i+"");
+//			ncov.setCountry(i+"");
+//			ncov.setProvince(i+"");
+//			data.add(ncov);
+//		}
+//		Global.Logger(this).info(ncovMapper.insertAll(data));
+
+		//-----------------------------------------------
+		URL url = new URL("https://raw.githubusercontent.com/canghailan/Wuhan-2019-nCoV/master/Wuhan-2019-nCoV.csv");
+		Reader reader = new InputStreamReader(new BOMInputStream(url.openStream()), StandardCharsets.UTF_8);
+		CSVParser parser = new CSVParser(reader, CSVFormat.EXCEL.withFirstRecordAsHeader());
+		try {
+			for (CSVRecord record : parser) {
+				Global.Logger(this).info(record.toString());
+			}
+		} finally {
+			parser.close();
+			reader.close();
 		}
-		Global.Logger(this).info(ncovMapper.insertAll(data));
 	}
 }
