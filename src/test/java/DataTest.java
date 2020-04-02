4d956f7a9654802722b0c630fa8ae1b53e56d303
ddv12138 <644077730@qq.om>
2020-04-02 14:38:32 +0800

    疫情数据处理

--- src/test/java/DataTest.java
+++ src/test/java/DataTest.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.io.IOException;
 import java.util.List;
 
 @SpringBootTest(classes = Application.class)
@@ -24,9 +25,8 @@ public class DataTest {
 	CommunityService communityService;
 
 	@Test
-	@Ignore
-	public void ncovTest() {
-		Global.doGetHttpRequest("https://raw.githubusercontent.com/canghailan/Wuhan-2019-nCoV/master/Wuhan-2019-nCoV.csv");
+	public void ncovTest() throws IOException {
+		Global.downLoadFromUrl("https://raw.githubusercontent.com/canghailan/Wuhan-2019-nCoV/master/Wuhan-2019-nCoV.json", "Wuhan-2019-nCoV.json", "G:\\");
 	}
 
 	@Test
