e1134e722fa9da007ac3b6f718b9e53f63de6dff
ddv12138 <644077730@qq.com>
2020-04-03 11:37:18 +0800

    配置https

--- src/main/java/GlobalUtils/Config/RootConfig.java
+++ src/main/java/GlobalUtils/Config/RootConfig.java
@@ -6,8 +6,16 @@ import com.alibaba.fastjson.support.config.FastJsonConfig;
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import com.alibaba.fastjson.support.spring.FastJsonRedisSerializer;
 import com.alibaba.fastjson.support.spring.GenericFastJsonRedisSerializer;
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.coyote.http11.Http11NioProtocol;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
@@ -121,5 +129,37 @@ class RootConfig {
 		fastConverter.setSupportedMediaTypes(supportedMediaTypes);
 		fastConverter.setFastJsonConfig(config);
 		return new HttpMessageConverters(fastConverter);
+	}
+
+	@Value("${server.port.http}")
+	private int serverPortHttp;
+
+	@Value("${server.port}")
+	private int serverPortHttps;
+
+	@Bean
+	public ServletWebServerFactory servletWebServerFactory() {
+		TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory() {
+			@Override
+			protected void postProcessContext(Context context) {
+				SecurityConstraint securityConstraint = new SecurityConstraint();
+				securityConstraint.setUserConstraint("CONFIDENTIAL");
+				SecurityCollection securityCollection = new SecurityCollection();
+				securityCollection.addPattern("/*");
+				securityConstraint.addCollection(securityCollection);
+				context.addConstraint(securityConstraint);
+			}
+		};
+		factory.addAdditionalTomcatConnectors(redirectConnector());
+		return factory;
+	}
+
+	private Connector redirectConnector() {
+		Connector connector = new Connector(Http11NioProtocol.class.getName());
+		connector.setScheme("http");
+		connector.setPort(serverPortHttp);
+		connector.setSecure(false);
+		connector.setRedirectPort(serverPortHttps);
+		return connector;
 	}
 }
