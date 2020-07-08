a563ec4ddf89a8c5a17a771bd00250fc7510ce18
ddv12138 <644077730@qq.com>
2020-07-08 17:16:19 +0800

    ElasticSearch

--- src/main/java/ddvudo/ScheduledTask/ElasticSearchTask.java
+++ src/main/java/ddvudo/ScheduledTask/ElasticSearchTask.java
@@ -1,6 +1,7 @@
 package ddvudo.ScheduledTask;
 
 import com.alibaba.fastjson.JSON;
+import ddvudo.GlobalUtils.Global;
 import ddvudo.ORM.Mapper.EnterpriseMapper;
 import ddvudo.ORM.POJO.Enterprise;
 import ddvudo.ORM.POJO.EnterpriseExample;
@@ -23,12 +24,12 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-@Profile("tencent-server")
+@Profile("tencentserver")
 @Component
 public class ElasticSearchTask {
 	private static final String ELASTICSEARCH_URL = "127.0.0.1";
 	private static final short ELASTICSEARCH_PORT = 9200;
-	private static final short MAX_POOL_SIZE = 200;
+	private static final short MAX_POOL_SIZE = 500;
 	private static final RestHighLevelClient client = new RestHighLevelClient(RestClient.builder(
 			new HttpHost(ELASTICSEARCH_URL, ELASTICSEARCH_PORT)));
 	Set<IndexRequest> requestSet = new HashSet<>();
@@ -37,7 +38,7 @@ public class ElasticSearchTask {
 	//	@Autowired
 //	MyBatisCursorItemReader<Enterprise> myBatisCursorItemReader;
 	@Resource
-	RedisTemplate<String, Long> redisTemplate;
+	RedisTemplate<String, String> redisTemplate;
 	long count = 0;
 
 	@Scheduled(fixedDelay = 1000L * 60L * 60L * 24L * 30L)
@@ -45,7 +46,7 @@ public class ElasticSearchTask {
 		EnterpriseExample example = new EnterpriseExample();
 		example.setOrderByClause("\"Id\"");
 		boolean stop = false;
-		for (int i = 0, j = 100; !stop; i += 100, j += 100) {
+		for (int i = 0, j = 100; !stop; i += 100) {
 			example.setOffset((long) i);
 			example.setLimit(j);
 			List<Enterprise> tmp = enterpriseMapper.selectByExample(example);
@@ -58,9 +59,9 @@ public class ElasticSearchTask {
 						.index("enterprise");
 				request.source(JSON.toJSONString(enterprise), XContentType.JSON);
 				requestSet.add(request);
-				if (requestSet.size() >= MAX_POOL_SIZE) {
-					this.handle();
-				}
+			}
+			if (requestSet.size() >= MAX_POOL_SIZE) {
+				this.handle();
 			}
 		}
 	}
@@ -94,10 +95,11 @@ public class ElasticSearchTask {
 		requestSet.forEach(bulkRequest::add);
 		try {
 			client.bulk(bulkRequest, RequestOptions.DEFAULT);
-			count += MAX_POOL_SIZE;
-			System.out.println(new Date().toString() + "--累计" + count + "条");
-			redisTemplate.opsForValue().set("已处理es数据累计", count);
+			count += requestSet.size();
+			Global.Logger(this).info(new Date().toString() + "--累计" + count + "条");
+			redisTemplate.opsForValue().set("已处理es数据累计", count + "");
 		} catch (Exception e) {
+			Global.Logger(this).error(e);
 			e.printStackTrace();
 		} finally {
 			requestSet.clear();
