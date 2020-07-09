0445de487043711a04761af7e7c884d5f09542d4
ddv12138 <644077730@qq.com>
2020-07-09 10:35:00 +0800

    zuul搞起来

---
+++ WebSite-zuul/src/main/java/ddvudo/zuul/zuulApplication.java
@@ -0,0 +1,13 @@
+package ddvudo.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class zuulApplication {
+	public static void main(String... args) {
+		SpringApplication.run(zuulApplication.class, args);
+	}
+}
