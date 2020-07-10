f3a8fc8b361677f426242a83228fbe6d2abf4aa6
ddv12138 <644077730@qq.com>
2020-07-10 17:36:47 +0800

    zuul和eureka还有config放一起

--- WebSite-eureka-config/src/main/java/ddvudo/eureka/EurekaApplication.java
+++ WebSite-eureka-config/src/main/java/ddvudo/eureka/EurekaApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
@@ -12,6 +13,7 @@ import org.springframework.context.annotation.Configuration;
 @EnableConfigServer
 @Configuration
 @EnableDiscoveryClient
+@EnableZuulProxy
 public class EurekaApplication {
 	public static void main(String... args) {
 		SpringApplication.run(EurekaApplication.class, args);
