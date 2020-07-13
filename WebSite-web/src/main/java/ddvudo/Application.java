fb7fce7e25cc5238d3d9a2fbc0dfb47f656e5617
ddv12138 <644077730@qq.com>
2020-07-13 16:09:05 +0800

    听说github挂了，试下push

--- WebSite-web/src/main/java/ddvudo/Application.java
+++ WebSite-web/src/main/java/ddvudo/Application.java
@@ -3,6 +3,7 @@ package ddvudo;
 import feign.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -13,6 +14,7 @@ import org.springframework.session.data.redis.config.annotation.web.http.EnableR
 @EnableRedisHttpSession(maxInactiveIntervalInSeconds = 3600)
 @SpringBootApplication
 @EnableFeignClients
+@EnableCircuitBreaker
 public class Application {
 	public static void main(String... args) {
 		SpringApplication.run(Application.class, args);
