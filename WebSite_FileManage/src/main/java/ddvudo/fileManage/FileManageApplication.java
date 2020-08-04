03e110246a9fbfd7a0418f8779f6de099e1f60cc
ddv12138 <644077730@qq.com>
2020-08-04 14:53:52 +0800

    关于文件的微服务

---
+++ WebSite_FileManage/src/main/java/ddvudo/fileManage/FileManageApplication.java
@@ -0,0 +1,11 @@
+package ddvudo.fileManage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileManageApplication {
+	public static void main(String... args) {
+		SpringApplication.run(FileManageApplication.class);
+	}
+}
